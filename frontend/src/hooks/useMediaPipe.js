8a984ff86863ca2d64d41a89c5d331d2fd0f7440
p3pp304 <fuzio.giuseppe2014@gmail.com>
2026-05-14 11:20:39 +0200

    miglioramento nella modulazione di MediaPipe.js

--- frontend/src/hooks/useMediaPipe.js
+++ frontend/src/hooks/useMediaPipe.js
@@ -10,7 +10,8 @@ import {
     X_SLEEP_THRESHOLD 
 } from '../utils/helpers';
 
-export const useMediaPipe = (videoRef, canvasRef, sessionStatus, onAlarm) => {
+export 
+    const useMediaPipe = (videoRef, canvasRef, sessionStatus, onAlarm) => {
     const [isSleeping, setIsSleeping] = useState(false);
     const [variableX, setVariableX] = useState(0);
 
@@ -18,10 +19,17 @@ export const useMediaPipe = (videoRef, canvasRef, sessionStatus, onAlarm) => {
     const closedStartTimeRef = useRef(null);
     const lastAlarmTimeRef = useRef(0);
 
-        // --- 2. MEDIAPIPE (EDGE COMPUTING) ---
+    // MEDIAPIPE (EDGE COMPUTING) ---
     useEffect(() => {
-        if (!videoRef.current || !canvasRef.current) return; // Sicurezza: se i riferimenti alla webcam o al canvas non sono pronti, esci dalla funzione
-        const canvasCtx = canvasRef.current.getContext('2d');  // Ottieni il contesto 2D del canvas per poter disegnare sopra la webcam
+        // Se non siamo in viaggio o mancano i riferimenti HTML, ferma tutto
+        if (sessionStatus !== "active" || !videoRef.current || !canvasRef.current) {
+            setVariableX(0);
+            setIsSleeping(false);
+            return;
+        }
+
+         // Ottieni il contesto 2D del canvas per poter disegnare sopra la webcam
+        const canvasCtx = canvasRef.current.getContext('2d'); 
 
         const faceMesh = new FaceMesh({
         locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`
@@ -29,42 +37,37 @@ export const useMediaPipe = (videoRef, canvasRef, sessionStatus, onAlarm) => {
         faceMesh.setOptions({ maxNumFaces: 1, refineLandmarks: true });  //
 
         faceMesh.onResults((results) => {
-        // Disegna la webcam e i landmark (se presenti)
-        canvasCtx.clearRect(0, 0, 1280, 720);
-        if (results.image) canvasCtx.drawImage(results.image, 0, 0, 1280, 720);
-        
-        if (results.multiFaceLandmarks?.[0]) {    // [0] perché stiamo monitorando solo un volto (il conducente); '?.' per sicurezza non blocca il programma se non rileva volti
-            const landmarks = results.multiFaceLandmarks[0];
-            const ear = (calculate_ear(landmarks, LEFT_EYE) + calculate_ear(landmarks, RIGHT_EYE)) / 2;
+                // Disegna la webcam e i landmark (se presenti)
+                canvasCtx.clearRect(0, 0, 1280, 720);
+                if (results.image) canvasCtx.drawImage(results.image, 0, 0, 1280, 720);
+                
+                if (results.multiFaceLandmarks?.[0]) {    // [0] perché stiamo monitorando solo un volto (il conducente); '?.' per sicurezza non blocca il programma se non rileva volti
+                    const landmarks = results.multiFaceLandmarks[0];
+                    const ear = (calculate_ear(landmarks, LEFT_EYE) + calculate_ear(landmarks, RIGHT_EYE)) / 2;
 
-            if (ear < EAR_THRESHOLD && sessionStatus === "active") {  // Se l'EAR è sotto la soglia e siamo in viaggio, considera gli occhi chiusi{
-            if (!closedStartTimeRef.current) {
-            closedStartTimeRef.current = performance.now();}
-            
-            const timeClosed = (performance.now() - closedStartTimeRef.current) / 1000;  // tempo da millisecondi a secondi
-            setVariableX(timeClosed.toFixed(2));
-            setIsSleeping(true);
-            
-            // Allarme se il tempo di chiusura supera la soglia e non abbiamo suonato l'allarme negli ultimi 2 secondi (2000ms, per evitare spam)
-            if (timeClosed > X_SLEEP_THRESHOLD && (performance.now() - lastAlarmTimeRef.current > 2000)) {
-                new Audio('/beep.mp3').play();   // Suona il file MP3
-                // Invia dati al server;
-                wsRef.current?.send(JSON.stringify({
-                event: "DROWSINESS_DETECTED",
-                variable_x: timeClosed.toFixed(2),
-                location: currentLocationRef.current || { lat: 41.1087, lng: 16.8784 } // Invia anche le coordinate GPS attuali al server per un possibile intervento proattivo
-                }));
-                lastAlarmTimeRef.current = performance.now();
+                    if (ear < EAR_THRESHOLD && sessionStatus === "active") {  // Se l'EAR è sotto la soglia e siamo in viaggio, considera gli occhi chiusi{
+                        if (!closedStartTimeRef.current) {
+                        closedStartTimeRef.current = performance.now();}
+                        
+                        const timeClosed = (performance.now() - closedStartTimeRef.current) / 1000;  // tempo da millisecondi a secondi
+                        setVariableX(timeClosed.toFixed(2));
+                        setIsSleeping(true);
+                
+                    // Allarme se il tempo di chiusura supera la soglia e non abbiamo suonato l'allarme negli ultimi 2 secondi (2000ms, per evitare spam)
+                    if (timeClosed > X_SLEEP_THRESHOLD && (performance.now() - lastAlarmTimeRef.current > 2000)) {
+                        playAlertBeep(); // Suona un beep di allarme
+                        onAlarm(timeClosed.toFixed(2)); // Passa il tempo di chiusura alla callback in App.jsx per aggiornare lo stato principale e inviare dati al server
+                        lastAlarmTimeRef.current = performance.now();
+                }
+                } else {
+                    // Azzera tutto se apre gli occhi
+                    closedStartTimeRef.current = null;
+                    setVariableX(0);
+                    setIsSleeping(false);
+                }
             }
-            } else {
-            // Azzera tutto se apre gli occhi
-            closedStartTimeRef.current = null;
-            setVariableX(0);
-            setIsSleeping(false);
-            }
-        }
-    });
-    // Avvia Webcam
+        });
+        // Avvia Webcam
         cameraRef.current = new Camera(videoRef.current, {
             onFrame: async () => await faceMesh.send({ image: videoRef.current }),
             width: 1280, height: 720
